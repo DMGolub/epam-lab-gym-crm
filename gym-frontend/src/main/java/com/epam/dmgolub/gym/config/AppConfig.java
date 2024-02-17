166faacf9630fcf72b351f7cbe64807066d43b3b
Dmitry Golub <dmgolub@mail.ru>
2024-02-18 01:54:56 +0400

    Eureka service discovery added

--- gym-frontend/src/main/java/com/epam/dmgolub/gym/config/AppConfig.java
+++ gym-frontend/src/main/java/com/epam/dmgolub/gym/config/AppConfig.java
@@ -1,5 +1,6 @@
 package com.epam.dmgolub.gym.config;
 
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
@@ -9,6 +10,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class AppConfig implements WebMvcConfigurer {
 
 	@Bean
+	@LoadBalanced
 	public RestTemplate restTemplate() {
 		return new RestTemplate();
 	}
