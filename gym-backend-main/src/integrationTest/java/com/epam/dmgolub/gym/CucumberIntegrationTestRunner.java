d58343a457318b00ee3f52330758ad2f995ec758
Dmitry Golub <dmgolub@mail.ru>
2024-04-07 23:00:12 +0400

    Added ability to run tests for a single endpoint from command line

--- gym-backend-main/src/integrationTest/java/com/epam/dmgolub/gym/CucumberIntegrationTestRunner.java
+++ gym-backend-main/src/integrationTest/java/com/epam/dmgolub/gym/CucumberIntegrationTestRunner.java
@@ -4,6 +4,10 @@ import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
+/*
+ To run tests for a single endpoint from command line use the following command:
+ ./gradlew clean integrationTest -PcucumberTags="@Endpoint-training-types" --info
+ */
 @RunWith(Cucumber.class)
 @CucumberOptions(features = "classpath:features/",
 	plugin = {"pretty"},
